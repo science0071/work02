5673d46a429c76ee6db019a5d3dbaa927eeaddc8
science0071 <106855787+science0071@users.noreply.github.com>
2023-08-14 23:34:51 +0330

    010

---
+++ 01/src/c01.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class c01 {
+
+	public static void main(String[] args) {
+		
+
+Scanner input = new Scanner(System.in);  
+		
+		int n ;
+		int r=0;
+		n=input.nextInt();
+		if (n<187)
+		{
+			
+			if(n%31==0)
+				r=n/31;
+			if(n%31!=0)
+				r=(n/31)+1;
+		 }
+		if (n<337 && n>=187)
+		{
+			
+			if((n-186)%30==0)
+				r=(n-186)/30+6;
+			if((n-186)%30!=0)
+				r=(n-186)/30+6+1;
+		 }
+		if(n>=337)
+			r=12;
+		System.out.print(r);
+	
+	
+	
+	
+	}//end main
+
+}// end class
